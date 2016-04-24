bf1210bc8aefc1e91afb14a242c336c584a72b40
Izhar Shaikh <izharits@gmail.com>
2016-04-24 15:15:11 -0400

    Fetching & Checking Database (Olivetti Faces)

---
+++ Source/EmotionRecognition.py
@@ -0,0 +1,27 @@
+"""
+======================================================
+Emotion recognition using SVMs (Scikit-learn & OpenCV
+======================================================
+
+Author: Izhar Shaikh
+License: MIT
+Dependencies: Python 2.7, Scikit-Learn, OpenCV 3.0.0,
+              Numpy, Scipy, Matplotlib
+Instructions: Please checkout Readme.txt & Instructions.txt
+
+The dataset used in this example is Olivetti Faces:
+ http://cs.nyu.edu/~roweis/data/olivettifaces.mat
+
+"""
+
+from pylab import *
+from sklearn import datasets
+
+faces = datasets.fetch_olivetti_faces()
+print faces.keys()
+
+for i in range(10):
+    face = faces.images[i]
+    subplot(1, 10, i + 1)
+    imshow(face.reshape((64, 64)), cmap='gray')
+    axis('off')
