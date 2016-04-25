eaeba964753f35e59b73e9fcf0886319633b0bf7
its-izhar <izharits@gmail.com>
2016-04-25 18:21:39 -0400

    Extracted the detected face to feed to SVC

--- source/FacePredict.py
+++ source/FacePredict.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from scipy.ndimage import zoom
 
 img = cv2.imread("../Data/Test.jpg")
 
@@ -9,16 +11,17 @@ detected_faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=6,
-        minSize=(100, 100),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+        minSize=(50, 50),
+        flags=cv2.CASCADE_SCALE_IMAGE)
 
-for (x,y,w,h) in detected_faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+for (x, y, w, h) in detected_faces:
+    cv2.rectangle(gray, (x, y), (x+w, y+h), (255,0,255), 3)
     roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+    horizontal_offset = int(0.15 * w)
+    vertical_offset = int(0.2 * h)
+    extracted_face = gray[y+vertical_offset : y+h, x+horizontal_offset : x-horizontal_offset+w]
 
-cv2.imshow('img', img)
+cv2.imshow('Extracted', extracted_face)
+cv2.imshow('gray', roi_gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
