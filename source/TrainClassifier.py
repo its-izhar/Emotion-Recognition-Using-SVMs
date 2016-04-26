d9269499a95abc60bc48320a28d5b6c14c38095c
its-izhar <izharits@gmail.com>
2016-04-25 23:13:50 -0400

    - OpenCV: Added Live Face Detection Based on Trained Classifier

--- source/TrainClassifier.py
+++ source/TrainClassifier.py
@@ -215,4 +215,53 @@ def test_recognition(c1, c2):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-test_recognition(0.3, 0.05)
+# test_recognition(0.3, 0.05)
+
+# ------------------- LIVE FACE RECOGNITION -----------------------------------
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # detect faces
+    gray, detected_faces = detectFaces(frame)
+
+    face_index = 0
+
+    # predict output
+    for face in detected_faces:
+        (x, y, w, h) = face
+        if w > 100:
+            # draw rectangle around face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            # extract features
+            extracted_face = extract_face_features(gray, face, (0.3, 0.05)) #(0.075, 0.05)
+
+            # predict smile
+            prediction_result = predict_face_is_smiling(extracted_face)
+
+            # draw extracted face in the top right corner
+            frame[face_index * 64: (face_index + 1) * 64, -65:-1, :] = cv2.cvtColor(extracted_face * 255, cv2.COLOR_GRAY2RGB)
+
+            # annotate main image with a label
+            if prediction_result is True:
+                cv2.putText(frame, "SMILING",(x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 155, 10)
+            else:
+                cv2.putText(frame, "not smiling",(x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 155, 10)
+
+            # increment counter
+            face_index += 1
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
